90fb668ca921fcee45de3c4dd9e6ceb76e78c088
russgards03 <99627318+russgards03@users.noreply.github.com>
2023-02-13 19:35:05 +0800

    Update db_file.sql

--- db_file.sql
+++ db_file.sql
@@ -12,19 +12,19 @@ CREATE TABLE `tbl_users` (
 );
 
 DROP TABLE IF EXISTS `tbl_productinv`;
-CREATE TABLE `tbl_productamt` (
+CREATE TABLE `tbl_productinv` (
   `product_id` int(180) unsigned NOT NULL auto_increment, 
   `product_qty` varchar(180) NOT NULL, 
   `batch_date` date NOT NULL default '0000-00-00',
   PRIMARY KEY  (`product_id`) 
 );
 
-DROP TABLE IF EXISTS `tbl_orderitems`;
+DROP TABLE IF EXISTS `tbl_products`;
 CREATE TABLE `tbl_products` (
   `order_id` int(8) unsigned NOT NULL auto_increment, 
   `product_id` int(8) NOT NULL, 
-  `order_qty` int(8) NOT NULL default '',
-  `order_amt` int(8) NOT NULL default '',
+  `order_qty` varchar(180) NOT NULL default '',
+  `order_amt` varchar(180) NOT NULL default '',
   KEY (`product_id`),
   PRIMARY KEY  (`order_id`)
 );
@@ -33,8 +33,8 @@ DROP TABLE IF EXISTS `tbl_orders`;
 CREATE TABLE `tbl_orders` (
   `order_id` int(8) unsigned NOT NULL auto_increment, 
   `order_date` date NOT NULL default '0000-00-00',
-  `total_orderamt` int(8) NOT NULL default '',
-  `total_orderqty` int(8) NOT NULL default '',
+  `total_orderamt` varchar(180) NOT NULL default '',
+  `total_orderqty` varchar(180) NOT NULL default '',
   KEY (`order_id`)
 );
 
@@ -43,8 +43,8 @@ CREATE TABLE `tbl_sales` (
   `sales_id` int(8) unsigned NOT NULL auto_increment, 
   `order_id` int(8) NOT NULL, 
   `order_date` date NOT NULL default '0000-00-00',
-  `sales_amt` int(8) NOT NULL default '',
-  `sales_qty` int(8) NOT NULL default '',
+  `sales_amt` varchar(180) NOT NULL default '',
+  `sales_qty` varchar(180) NOT NULL default '',
   PRIMARY KEY (`sales_id`),
   KEY (`order_id`)
 );
