6405353997d9dfa1491c12435ff09aace919f92b
russgards03 <99627318+russgards03@users.noreply.github.com>
2023-02-14 21:17:00 +0800

    Update db_file.sql

--- db_file.sql
+++ db_file.sql
@@ -7,6 +7,11 @@ CREATE TABLE `tbl_users` (
   `user_firstname` varchar(180) NOT NULL default '',
   `user_email` varchar(180) NOT NULL default '',
   `user_password` varchar(180) NOT NULL default '',
+  `user_date_added` date NOT NULL default '0000-00-00',
+  `user_time_added` time NOT NULL default '00:00:00',	
+  `user_date_updated` date NOT NULL default '0000-00-00',
+  `user_time_updated` time NOT NULL default '00:00:00',
+  `user_status` int(1) NOT NULL default '0',
   `user_access` varchar(180) NOT NULL default '',
   PRIMARY KEY  (`user_id`)
 );
